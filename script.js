1efb59213fb3f692699b78af99766b5959c6515e
mariam <mariamabdelhaq883@gmail.com>
2025-06-28 15:48:22 +0300

    add menu list

--- script.js
+++ script.js
@@ -58,3 +58,19 @@ btns.forEach(btn => {
     courses.style.display= "block";
 })
 })
+
+const menu = document.getElementById("mobile-menu");
+const menubtn= document.getElementById("menu-icon");
+const menua =document.querySelectorAll(".mobile-menu a");
+const animation ="animation-menu";
+menubtn.addEventListener("click", () => {
+ if (menu.style.display === "flex") {
+    menu.style.display = "none";
+    menua.forEach(link =>{link.style.display="none"});
+  } 
+  else {
+    menu.style.display = "flex";
+    menua.forEach(link =>{link.style.display="flex"});
+    menu.classList.add(animation);
+  }
+})
